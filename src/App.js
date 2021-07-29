006878d93bb5bc2ab0006d1e382c851eac8d0473
Aditya Prasad Mishra <ronaldo.aditya.07@gmail.com>
2021-07-29 16:41:18 +0530

    Adding Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Container,Card,CardContent,makeStyles,Grid,TextField,Button,Divider } fr
 import qrcode from 'qrcode';
 import QrReader from 'react-qr-reader';
 
+
 function App() {
   const classes= useStyles();
   let [text,setText]=react.useState("");
@@ -50,7 +51,7 @@ const handleScanWebCam = (result) => {
     <Container className={classes.container}>
 
       <Card>
-          <h2 >GENERATE / DOWNLOAD / SCAN</h2>
+          <h2 className={classes.title}>GENERATE / DOWNLOAD / SCAN</h2>
           <CardContent>
 
               <Grid  container spacing={4}>
@@ -85,7 +86,7 @@ const handleScanWebCam = (result) => {
                 </Grid>
 
                 <Grid  item xl={4} lg={4} md={6} sm={12}>
-
+                  <h5 className={classes.G3Text}>SCAN BY WEBCAM</h5>
                 <QrReader
                    delay={300}
                    style={{width: '100%'}}
@@ -102,8 +103,25 @@ const handleScanWebCam = (result) => {
           </CardContent>
 
       </Card>
+      
+      <footer className={classes.footer}>
+      <p>Made By :</p>
+      <p className={classes.footerName}>
+         Aditya Prasad Mishra       
+      </p>
+      </footer>
+     
+
+      <div className={classes.footerIcons}>
+      <a href="https://github.com/adimishrax7x"><i className={classes.Icon} class="fab fa-github fa-2x"></i></a>
+      
+      <a href="https://www.linkedin.com/in/aditya-prasad-mishra-9555571a5/"><i className={classes.Icon} class="fab fa-linkedin fa-2x"></i></a>
+      </div>
+
 
     </Container>
+
+    
   );
 }
 
@@ -140,6 +158,28 @@ const useStyles = makeStyles((themes)=>({
     color: 'grey',
     textDecoration:'underline white',
     textAlign:'center'
+  },
+  G3Text:{
+    color:'#F8485E',
+    display :'flex',
+    justifyContent :'center'
+  },
+  footerName:{
+    color:'#F8485E',
+    display :'flex',
+    
+  },
+  footer:{
+    display :'flex',
+    justifyContent :'center'
+  },
+  footerIcons:{
+    display :'flex',
+    justifyContent :'center'
+  },
+  Icon:{
+    background:'#F8485E',
+    marginLeft:'10%'
   }
 
 }))
